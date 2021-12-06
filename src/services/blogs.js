c76fe1052cab8c4f39806b0c7e188bd6693c308b
Emma Hardin <emma@Emmas-MacBook-Pro-2.local>
2021-12-06 14:23:50 -0800

    adding author to blog card

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,6 +1,6 @@
 import { checkError, client } from './client';
 
 export async function getBlogs() {
-  const response = await client.from('blogs').select();
+  const response = await client.from('blogs').select('*, authors:author_id(id, name)');
   return checkError(response);
 }
