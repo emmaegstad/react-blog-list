e847cb273519d135e55b4ebad765f979cac7c2b4
Emma Hardin <emma@Emmas-MacBook-Pro-2.local>
2021-12-06 16:30:02 -0800

    trying to fix test

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -7,7 +7,7 @@ export default function BlogCard({ title, subtitle, text, image, authors: { name
       <div className="details">
         <div className="title">{title}</div>
         <div className="subtitle">{subtitle}</div>
-        <div className="author">Written by: {name}</div>
+        <div className="author">Written By: {name}</div>
         <div className="text">{text}</div>
       </div>
       <div className="image">
